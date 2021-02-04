e58d8b02641c686f197ec6b0d72e9efb1ec90de7
JLP0578 <54902345+JLP0578@users.noreply.github.com>
2021-02-04 16:11:22 +0100

    Factory v5

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -31,7 +31,6 @@ $factory->define(User::class, function (Faker $faker) {
 
         'created_at' => $faker->dateTime,
         'updated_at' => $faker->dateTime,
-        'deleted_at' => '',
     ];
 });
 
