0954994122cd81a49aff4ebce48d9dc3dd4077a0
JLP0578 <54902345+JLP0578@users.noreply.github.com>
2021-02-04 15:35:04 +0100

    Factory v5

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -18,12 +18,20 @@ use Faker\Generator as Faker;
 
 $factory->define(User::class, function (Faker $faker) {
     return [
-        'nomUser' => $faker->name,
-        'prenomUser' => $faker->lastName,
-        'mailUser' => $faker->unique()->safeEmail,
-        'mdpUser' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
-        'adresseUser' => 'Jean Eude',
+        'nom' => $faker->firstName,
+        'prenom' => $faker->lastName,
+        'email' => $faker->unique()->safeEmail,
+        'mdp' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
+        'adresse' => 'Jean Eude',
+        'prefixtel' => '+33',
+        'tel' => $faker->numerify('##########'),
+        'role' => random_int(1,3),
+
         'city_id' => random_int(1,4),
+
+        'created_at' => $faker->dateTime,
+        'updated_at' => $faker->dateTime,
+        'deleted_at' => '',
     ];
 });
 
