cd44b80435a36a82e359586ef933105f0e11bab5
Théo Meozzi <theomeozzi.pro@gmail.com>
2021-02-11 12:04:10 +0100

    factory product & shop corrigées

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -7,12 +7,12 @@ use Faker\Generator as Faker;
 
 $factory->define(Product::class, function (Faker $faker) {
     return [
-        'name' => $faker->name,
-        'description' => $faker->description,
-        'reference' => $faker->reference,
-        'price' => $faker->price,
-        'shops_id' => factory(App\Shop::class),
-        'unit_id' => factory(App\Unit::class),
+        'libelle' => $faker->text,
+        'description' => $faker->text,
+        'reference' => $faker->text,
+        'prix' => $faker->random_int(1, 50),
+        'shops_id' => $faker->random_int(1, 20),
+        'unit_id' => $faker->random_int(1, 5),
 
         'created_at' => $faker->dateTime,
         'updated_at' => $faker->dateTime
