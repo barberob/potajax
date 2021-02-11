e2a3b6064213ffd39956683302fe8281b54b6328
michelgege <50527053+michelgege@users.noreply.github.com>
2021-02-11 14:54:02 +0100

    add leaflet

--- resources/js/pages/RegisterForm.js
+++ resources/js/pages/RegisterForm.js
@@ -15,15 +15,15 @@ export default class RegisterForm {
     }
 
     initEvents() {
-        this.els.managerButton.addEventListener('click', (event) => {
+        this.els.managerButton.addEventListener('click', () => {
             this.els.managerInputsContainer.classList.toggle('hidden')
             if (this.els.managerButton.checked) {
-                [...this.els.managerInputsContainer.querySelector('input')].forEach(input => {
+                this.els.managerInputsContainer.querySelectorAll('input').forEach(input => {
                     input.removeAttribute('disabled')
                 })
             } else {
-                [...this.els.managerInputsContainer.querySelector('input')].forEach(input => {
-                    input.setAttribute('disabled')
+                this.els.managerInputsContainer.querySelectorAll('input').forEach(input => {
+                    input.setAttribute('disabled', '')
                 })
             }
         })
