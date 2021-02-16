98354150c5340fe095b079da1807551bbdb5b66a
michelgege <50527053+michelgege@users.noreply.github.com>
2021-02-16 11:32:14 +0100

    keyboard navigation

--- resources/js/pages/RegisterForm.js
+++ resources/js/pages/RegisterForm.js
@@ -1,14 +1,15 @@
-
 export default class RegisterForm {
 
     constructor() {
-        this.config = {
-            api_endpoint : 'https://api-adresse.data.gouv.fr/search/?q='
-        }
         if(document.querySelector('body.register')) {
             this.initEls()
             this.initEvents()
+        } else return
+
+        this.config = {
+            api_endpoint : 'https://api-adresse.data.gouv.fr/search/?q='
         }
+        this.selectedItem = 0
     }
 
     initEls() {
@@ -17,7 +18,7 @@ export default class RegisterForm {
             managerInputsContainer : document.querySelector('.js-manager-inputs'),
             inputAutoComplete : document.querySelector('.js-adress'),
             autoCompleteContainer : document.querySelector('.js-autocomplete'),
-            autoCompleteItems : null
+            autoCompleteItems : 0
         }
     }
 
@@ -28,12 +29,13 @@ export default class RegisterForm {
 
     initManagerForm() {
         this.els.managerButton.addEventListener('click', () => {
-            this.els.managerInputsContainer.classList.toggle('hidden')
             if (this.els.managerButton.checked) {
+                this.els.managerInputsContainer.classList.remove('hidden')
                 this.els.managerInputsContainer.querySelectorAll('input').forEach(input => {
                     input.removeAttribute('disabled')
                 })
             } else {
+                this.els.managerInputsContainer.classList.add('hidden')
                 this.els.managerInputsContainer.querySelectorAll('input').forEach(input => {
                     input.setAttribute('disabled', '')
                 })
@@ -42,31 +44,73 @@ export default class RegisterForm {
     }
 
     initAutoComplete() {
-        console.log('attach event');
         this.els.inputAutoComplete.addEventListener('input', () =>  {
             this._autoComplete().catch(e => console.log(e))
         })
         this.els.inputAutoComplete.addEventListener('blur', (e) =>  {
-            e.preventDefault()
             this.els.autoCompleteContainer.classList.add('js-hidden')
+        })
+        this.els.inputAutoComplete.addEventListener('focus', (e) =>  {
+            this.els.autoCompleteContainer.classList.add('js-hidden')
+        })
+        this.els.inputAutoComplete.addEventListener('keydown', event => {
+            console.log(event.key);
+            if (this.els.autoCompleteItems.length === 0) return
+            if(event.key === 'ArrowDown' && this.selectedItem < this.els.autoCompleteItems.length) {
+                this.selectedItem++
+            }
+            else if (event.key === 'ArrowUp' && this.selectedItem > 0) {
+                this.selectedItem--
+            }
+            if (event.key === 'Enter') {
+                event.preventDefault()
+                try {
+                    const value = document.querySelector(`ul.js-autocomplete li:nth-of-type(${this.selectedItem})`).textContent
+                    this.els.inputAutoComplete.value = value
+                    this._handleView(false)
+                } catch (e) {}
+                return
+            }
+            try {
+                document.querySelectorAll(`ul.js-autocomplete li`).forEach((item, i) => {
+                    if (i === this.selectedItem - 1) item.classList.add('js-active')
+                    else item.classList.remove('js-active')
+                })
+            } catch (e) {}
         })
     }
 
+    _listenKeyEvents(event) {
+        if(event.key === 'ArrowDown' && this.selectedItem < this.els.autoCompleteItems.length) {
+            this.selectedItem++
+        }
+        else if (event.key === 'ArrowUp' && this.selectedItem > 0) {
+            this.selectedItem--
+        }
+        console.log(this.selectedItem)
+    }
+
     async _autoComplete() {
-        this.els.autoCompleteContainer.classList.remove('js-hidden')
         const value = this.els.inputAutoComplete.value
+
+        if(!value) {
+            this._handleView(false)
+            return
+        }
+
+        this.els.autoCompleteContainer.classList.remove('js-hidden')
         const query = value.split(' ').join('+')
 
         const source = await fetch(this.config.api_endpoint + query)
         const res = await source.json()
-        if (res.features.length === 0) return
+
+        if (res.features.length === 0) this._handleView('false')
         const formatted_res = res.features.map(adress => adress.properties.label)
 
         this._createList(formatted_res)
     }
 
     _createList(data) {
-        console.log(data);
         if(data.length === 0) this._handleView()
         this.els.autoCompleteContainer.innerHTML = ''
         data.forEach(result => {
@@ -78,7 +122,7 @@ export default class RegisterForm {
 
     _createListItem(result) {
         const li = document.createElement('li')
-        li.addEventListener('click', e => {
+        li.addEventListener('mousedown', e => {
             this.els.autoCompleteContainer.classList.add('js-hidden')
             if(this.els.autoCompleteItems) {
                 this.els.inputAutoComplete.value = e.currentTarget.textContent
@@ -93,3 +137,4 @@ export default class RegisterForm {
         else this.els.autoCompleteContainer.classList.remove('js-hidden')
     }
 }
+
