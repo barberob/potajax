a92f63ebbf2557f443e8d5231d2fb5188606f280
Claire-Sabatier <claire.sabatier@etu.umontpellier.fr>
2021-02-16 10:06:51 +0100

    pages shop v2

--- resources/views/pages/shop.blade.php
+++ resources/views/pages/shop.blade.php
@@ -2,11 +2,13 @@
 
 @section('content')
 
-<div class="name">
-	<h2 class="title">{{ $infos->nom }}</h2>
-	<p class="adresse">{{$infos->adresse}}</p>
-	<p class="tel">Téléphone: {{$infos->tel}}</p>
-	<p class="mail">@: {{$infos->email}}</p>
+<div class="name" style="background-image:url('../img/{{$img}}.jpg');">
+	<div>
+		<h2 class="title">{{ $infos->nom }}</h2>
+		<p class="adresse">{{$infos->adresse}}</p>
+		<p class="tel">Téléphone: {{$infos->tel}}</p>
+		<p class="mail">@: {{$infos->email}}</p>
+	</div>
 </div>
 <div class="horaires">
 	<p> Ouvert les : {{$infos->horaires}}</p>
@@ -14,6 +16,8 @@
 <div class="descriptif">
 	<p>{{$infos->descriptif}}</p>
 </div>
+
+
 
 
 
