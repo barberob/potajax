bedb586536f457f3b53d59676b591dcfd0795c54
Claire-Sabatier <claire.sabatier@etu.umontpellier.fr>
2021-02-16 11:26:04 +0100

    réécriture de certains controller en utilisant les models

--- app/Http/Controllers/SubcategoriesController.php
+++ app/Http/Controllers/SubcategoriesController.php
@@ -3,25 +3,20 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
 use App\Shops\Categorie;
+use App\Shops\Shop;
+use App\Shops\SubCategorie;
+
 class SubcategoriesController extends Controller
 {
     public function listSubcat($category_id){
-		$subcategories = DB::table('subcategories')
-		->distinct()
-		->where("category_id","=",$category_id)
-		->get();
+		$subcategories = SubCategorie::where('category_id',$category_id)->get(); 
 
-		$categories = DB::table('categories')->distinct()->get();
+		$categories = Categorie::all();
 
 		$current_category = Categorie::findOrFail($category_id)->libelle;
 
-		$shops = DB::table('shops')
-		->distinct()
-		->where("category_id",'=',$category_id)
-		->get();
-
+		$shops = Shop::where("category_id",$category_id)->get();
 
 		return view('pages.map', [
             'subcategories' => $subcategories,
