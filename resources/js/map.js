b4521d17fe880fb7085dbcea005cb00916cc0072
Claire-Sabatier <claire.sabatier@etu.umontpellier.fr>
2021-03-09 12:04:24 +0100

    popup map

--- resources/js/map.js
+++ resources/js/map.js
@@ -177,8 +177,8 @@ export default class Map{
             let icone = null;
             let color = null;
 
-            data += '<p>Nom: '+Item.detail['nom']+'</p>';
-            data += '<p>Desc: '+Item.detail['desc']+'</p>';
+            data += '<p style="font-weight:bold; font-size:18px; color:#EE8E6B;">'+Item.detail['nom']+'</p>';
+            data += '<p style="font-size:15px;">'+Item.detail['desc']+'</p>';
 
             //marker = L.marker([Item.coord['Lat'], Item.coord['Lng']],/* {icon: IconWhite}*/).bindPopup(data);
 
