f527c5da420954e1aa72a0cdaa93e4432e0d662a
Théo Meozzi <theomeozzi.pro@gmail.com>
2021-03-25 10:25:41 +0100

    fix stats + visites totales

--- resources/views/pages/stats.blade.php
+++ resources/views/pages/stats.blade.php
@@ -5,6 +5,13 @@
     @manager
         <script src="https://cdn.jsdelivr.net/npm/chart.js@2.8.0"></script>
 
+        <div class="card" style="width: 400px;">
+		  <div class="card-body">
+		    <h2 class="card-title text-center"> Nombre total de visites </h2>
+		    <h3 class="card-text text-center mt-4"> {{ $allVisits }} </h3>
+		  </div>
+		</div>
+
         <div class="divContainer">
         	<canvas id="chart"></canvas>
         </div>
@@ -12,11 +19,11 @@
 	    <script type="text/javascript">
 	    	let myChart = document.getElementById('chart').getContext('2d');
 
-	    	var consultations = @json($consultations);
+	    	var visits = @json($visits);
 
-	    	console.log(consultations);
+	    	console.log(visits);
 
-        	var months = ["January", "February", "March", "April", "June", "July", "August", "September", "October", "November", "December"];
+        	var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
         	var currentMonth = new Date().getMonth();
 
@@ -24,21 +31,29 @@
 
         	console.log(orderedMonth);
 
-        	console.log(orderedMonth.map((month)=> {return consultations?.[month]}));
+        	console.log(orderedMonth.map((month)=> {return visits?.[month]}));
 
 	    	let lineChart = new Chart(myChart, {
 	    		type: 'line',
 	    		data: {
 	    			labels: orderedMonth,
 	    			datasets: [{
-	    				label: 'Nombre de visites',
-	    				data: orderedMonth.map((month)=> {return consultations?.[month]}),
+	    				label: 'Nombre de visites par mois',
+	    				data: orderedMonth.map((month)=> {return visits?.[month]}),
 	    				fill: false,
                         borderColor: "rgb(255, 0, 0)",
                         lineTension: 0.1
 	    			}]
 	    		},
-	    		options: {}
+	    		options: {
+	    			scales: {
+	    				yAxes: [{
+	    					ticks: {
+	    						beginAtZero: true
+	    					}
+	    				}]
+	    			}
+	    		}
 	    	});
 	    </script>
     @endmanager
